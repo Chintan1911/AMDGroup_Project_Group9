70999b07fe27c425f3db0d9e710604014c179b9b
Chintan1911 <chintan191199@gmail.com>
2024-03-12 13:54:05 -0400

    RegistrationActivity.kt and its XML created to register user in firebase with name, email and password.

--- app/src/main/java/com/example/jobfinder/RegistrationActivity.kt
+++ app/src/main/java/com/example/jobfinder/RegistrationActivity.kt
@@ -1,11 +1,61 @@
 package com.example.jobfinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class RegistrationActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var registerButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        auth = FirebaseAuth.getInstance()
+
+        emailEditText = findViewById(R.id.email)
+        passwordEditText = findViewById(R.id.password)
+        registerButton = findViewById(R.id.button)
+
+        registerButton.setOnClickListener {
+            createAccount()
+        }
+    }
+    private fun createAccount() {
+        val email = emailEditText.text.toString().trim()
+        val password = passwordEditText.text.toString().trim()
+
+        if (email.isEmpty()) {
+            Toast.makeText(this, "Please enter email.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (password.isEmpty()) {
+            Toast.makeText(this, "Please enter password.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // Firebase authentication to create a user with email and password
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(this, "Registration successful.", Toast.LENGTH_SHORT).show()
+
+                    // After successful registration, redirect the user to the LoginActivity
+                    startActivity(Intent(this@RegistrationActivity, LoginActivity::class.java))
+                    finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this, "Registration failed: ${task.exception?.message}",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
